44a3da215176b20d0510dfc00418c231c612ac21
voffie <viktorsoderborg@gmail.com>
2025-03-20 15:33:37 +0100

    Initial commit

---
+++ src/main/java/org/storkforge/barkr/BarkrApplication.java
@@ -0,0 +1,13 @@
+package org.storkforge.barkr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarkrApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BarkrApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/storkforge/barkr/BarkrApplicationTests.java
@@ -0,0 +1,13 @@
+package org.storkforge.barkr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarkrApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
