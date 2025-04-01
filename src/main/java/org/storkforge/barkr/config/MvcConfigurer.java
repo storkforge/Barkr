a4434b94ea02c7cdb07b70cf022dd8119432ff41
voffie <viktorsoderborg@gmail.com>
2025-04-01 20:19:16 +0200

    fix: remove unused import

--- src/main/java/org/storkforge/barkr/config/MvcConfigurer.java
+++ src/main/java/org/storkforge/barkr/config/MvcConfigurer.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
