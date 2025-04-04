13723287bd1afe64e115fab85d2c0fcad7f75689
voffie <viktorsoderborg@gmail.com>
2025-04-04 14:59:48 +0200

    feat: add locale support

--- src/main/java/org/storkforge/barkr/config/MvcConfigurer.java
+++ src/main/java/org/storkforge/barkr/config/MvcConfigurer.java
@@ -1,21 +1,19 @@
 package org.storkforge.barkr.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
-import java.util.Locale;
-
+@Configuration
 public class MvcConfigurer implements WebMvcConfigurer {
 
   @Bean
   public LocaleResolver localeResolver() {
-    SessionLocaleResolver localeResolver = new SessionLocaleResolver();
-    localeResolver.setDefaultLocale(Locale.getDefault());
-    return localeResolver;
+    return new SessionLocaleResolver();
   }
 
   @Bean
