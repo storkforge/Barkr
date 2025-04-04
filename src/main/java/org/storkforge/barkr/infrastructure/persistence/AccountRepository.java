d0ff21cead225f4832abf2a054bfc426d00cf76d
voffie <viktorsoderborg@gmail.com>
2025-04-04 12:44:30 +0200

    feat: add find functions to repositories

--- src/main/java/org/storkforge/barkr/infrastructure/persistence/AccountRepository.java
+++ src/main/java/org/storkforge/barkr/infrastructure/persistence/AccountRepository.java
@@ -3,6 +3,8 @@ package org.storkforge.barkr.infrastructure.persistence;
 import org.springframework.data.repository.ListCrudRepository;
 import org.storkforge.barkr.domain.entity.Account;
 
-public interface AccountRepository extends ListCrudRepository<Account, Long> {
+import java.util.Optional;
 
+public interface AccountRepository extends ListCrudRepository<Account, Long> {
+  Optional<Account> findByUsernameEqualsIgnoreCase(String username);
 }
--- src/main/java/org/storkforge/barkr/infrastructure/persistence/PostRepository.java
+++ src/main/java/org/storkforge/barkr/infrastructure/persistence/PostRepository.java
@@ -1,8 +1,11 @@
 package org.storkforge.barkr.infrastructure.persistence;
 
 import org.springframework.data.repository.ListCrudRepository;
+import org.storkforge.barkr.domain.entity.Account;
 import org.storkforge.barkr.domain.entity.Post;
 
-public interface PostRepository extends ListCrudRepository<Post, Long> {
+import java.util.List;
 
+public interface PostRepository extends ListCrudRepository<Post, Long> {
+  public List<Post> findByAccount(Account account);
 }
