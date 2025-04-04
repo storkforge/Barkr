0c2af8d4a503b64c3a2c02ef2036ae72f1b81e31
voffie <viktorsoderborg@gmail.com>
2025-04-04 12:46:12 +0200

    feat: add support in controller for profile page

--- src/main/java/org/storkforge/barkr/web/controller/WebController.java
+++ src/main/java/org/storkforge/barkr/web/controller/WebController.java
@@ -4,8 +4,11 @@ import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.storkforge.barkr.domain.AccountService;
 import org.storkforge.barkr.domain.PostService;
 import org.storkforge.barkr.domain.entity.Post;
+import org.storkforge.barkr.dto.accountDto.ResponseAccount;
+import org.storkforge.barkr.dto.postDto.CreatePost;
 
 import java.util.List;
 
@@ -14,9 +17,11 @@ import java.util.List;
 public class WebController {
 
   private final PostService postService;
+  private final AccountService accountService;
 
-  public WebController(PostService postService) {
+  public WebController(PostService postService, AccountService accountService) {
     this.postService = postService;
+    this.accountService = accountService;
   }
 
   @GetMapping("/")
@@ -26,6 +31,25 @@ public class WebController {
     return "index";
   }
 
+  @GetMapping("/{username}")
+  public String user(@PathVariable("username") String username, Model model) {
+    ResponseAccount queryAccount = accountService.findByUsername(username);
+    if (queryAccount == null) {
+      return "redirect:/";
+    }
+
+    model.addAttribute("accountPosts", postService.findByUsername(username));
+    model.addAttribute("queryAccount", queryAccount);
+    // TODO: Change this to the actual account once security is in place
+    model.addAttribute("account", accountService.findById(1L));
+
+    return "profile";
+  }
+
+  @PostMapping("/post/add")
+  public String addPost(@ModelAttribute CreatePost dto,
+                        RedirectAttributes redirectAttributes) {
+    postService.addPost(dto);
 
 }
 
