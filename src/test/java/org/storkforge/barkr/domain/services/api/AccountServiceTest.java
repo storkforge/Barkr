e9b6a0dbc090723d933a137ffa6234ac41aa3bb7
efpcode <efpjobhunt@gmail.com>
2025-04-06 23:04:58 +0200

    (fix) covers empty hit with account service

--- src/test/java/org/storkforge/barkr/domain/services/api/AccountServiceTest.java
+++ src/test/java/org/storkforge/barkr/domain/services/api/AccountServiceTest.java
@@ -36,7 +36,7 @@ class AccountServiceTest {
         @Test
         @DisplayName("No accounts in database throws exception")
         void noAccountsInDatabaseThrowsException() {
-            when(accountRepository.findAll()).thenThrow(new AccountNotFound("No account record(s) found in database"));
+            when(accountRepository.findAll()).thenReturn(List.of()).thenThrow(new AccountNotFound("No account record(s) found in database"));
             assertThatThrownBy(accountService::findAll).isInstanceOf(AccountNotFound.class).hasMessage("No account record(s) found in database");
 
         }
@@ -60,59 +60,57 @@ class AccountServiceTest {
         }
     }
 
-        @Nested
-        class AccountsTests{
-
-            @Test
-            @DisplayName("findAll returns all users in database")
-            void findAllReturnsAllUsersInDatabase() {
-                Account mockAccount = mock(Account.class);
-                Account mockAccount2 = mock(Account.class);
-                when(mockAccount.getId()).thenReturn(1L);
-                when(mockAccount2.getId()).thenReturn(2L);
-                when(mockAccount.getUsername()).thenReturn("John Doe");
-                when(mockAccount2.getUsername()).thenReturn("Jane Doe");
-                when(mockAccount.getCreatedAt()).thenReturn(LocalDateTime.now());
-                when(mockAccount2.getCreatedAt()).thenReturn(LocalDateTime.now());
-
-                List<Account> mockAccounts = List.of(mockAccount, mockAccount2);
-                when(accountRepository.findAll()).thenReturn(mockAccounts);
-
-                var result = accountService.findAll();
-
-                assertThat(result).hasSize(2);
-                assertThat(result.getFirst()).extracting("username").isEqualTo("John Doe");
-                verify(accountRepository, times(1)).findAll();
-
-            }
-
-
-
-        }
-
+    @Nested
+    class AccountsTests {
 
         @Test
-        @DisplayName("findOne returns valid account when valid id is passed")
-        void findOneReturnsValidAccountWhenValidIdIsPassed() {
-
-            LocalDateTime constantTime = LocalDateTime.of(2020, 1, 1, 1, 1);
-
+        @DisplayName("findAll returns all users in database")
+        void findAllReturnsAllUsersInDatabase() {
             Account mockAccount = mock(Account.class);
+            Account mockAccount2 = mock(Account.class);
             when(mockAccount.getId()).thenReturn(1L);
+            when(mockAccount2.getId()).thenReturn(2L);
             when(mockAccount.getUsername()).thenReturn("John Doe");
-            when(mockAccount.getCreatedAt()).thenReturn(constantTime);
+            when(mockAccount2.getUsername()).thenReturn("Jane Doe");
+            when(mockAccount.getCreatedAt()).thenReturn(LocalDateTime.now());
+            when(mockAccount2.getCreatedAt()).thenReturn(LocalDateTime.now());
 
-            when(accountRepository.findById(eq(1L))).thenReturn(Optional.of(mockAccount));
+            List<Account> mockAccounts = List.of(mockAccount, mockAccount2);
+            when(accountRepository.findAll()).thenReturn(mockAccounts);
 
-            var result = accountService.findOne(1L);
+            var result = accountService.findAll();
 
-            assertThat(result).extracting("username").isEqualTo("John Doe");
-            assertThat(result).extracting("id").isEqualTo(1L);
-            assertThat(result).extracting("createdAt").isEqualTo(constantTime);
-            verify(accountRepository, times(1)).findById(eq(1L));
+            assertThat(result).hasSize(2);
+            assertThat(result.getFirst()).extracting("username").isEqualTo("John Doe");
+            verify(accountRepository, times(1)).findAll();
 
         }
-
 
 
     }
+
+
+    @Test
+    @DisplayName("findOne returns valid account when valid id is passed")
+    void findOneReturnsValidAccountWhenValidIdIsPassed() {
+
+        LocalDateTime constantTime = LocalDateTime.of(2020, 1, 1, 1, 1);
+
+        Account mockAccount = mock(Account.class);
+        when(mockAccount.getId()).thenReturn(1L);
+        when(mockAccount.getUsername()).thenReturn("John Doe");
+        when(mockAccount.getCreatedAt()).thenReturn(constantTime);
+
+        when(accountRepository.findById(eq(1L))).thenReturn(Optional.of(mockAccount));
+
+        var result = accountService.findOne(1L);
+
+        assertThat(result).extracting("username").isEqualTo("John Doe");
+        assertThat(result).extracting("id").isEqualTo(1L);
+        assertThat(result).extracting("createdAt").isEqualTo(constantTime);
+        verify(accountRepository, times(1)).findById(eq(1L));
+
+    }
+
+
+}
