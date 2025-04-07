3f61bef4dc84b933dda547b323cbb48e21932f7a
voffie <viktorsoderborg@gmail.com>
2025-04-07 14:53:26 +0200

    fix: set default locale

--- src/main/java/org/storkforge/barkr/config/MvcConfigurer.java
+++ src/main/java/org/storkforge/barkr/config/MvcConfigurer.java
@@ -8,12 +8,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+import java.util.Locale;
+
 @Configuration
 public class MvcConfigurer implements WebMvcConfigurer {
 
   @Bean
   public LocaleResolver localeResolver() {
-    return new SessionLocaleResolver();
+    SessionLocaleResolver slr = new SessionLocaleResolver();
+    slr.setDefaultLocale(Locale.ENGLISH);
+    return slr;
   }
 
   @Bean
