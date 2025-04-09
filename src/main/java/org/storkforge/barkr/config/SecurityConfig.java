5f7c911fa9c576d50d2f9f93f205d6b1adc10d03
efpcode <efpjobhunt@gmail.com>
2025-04-09 18:43:20 +0200

    (feature) Adds new dependency and initial SecurityConfig file
    Co-authored-by: Sedya seyda.kinaci@iths.se>

---
+++ src/main/java/org/storkforge/barkr/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package org.storkforge.barkr.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .formLogin(Customizer.withDefaults())
+                //.oauth2Login(Customizer.withDefaults())
+                .authorizeHttpRequests(authorize -> authorize
+                                .requestMatchers("/","/login", "/error").permitAll()
+                                .requestMatchers("/profile/**").authenticated()
+                                .requestMatchers("/post/add").authenticated()
+                                .requestMatchers("/account/**/*").authenticated()
+                                .requestMatchers("/**/{username}").authenticated()
+                                .anyRequest().denyAll()
+
+                        );
+        return http.build();
+    }
+ }
