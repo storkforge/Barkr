3e74dfed068126e013792d44dc52ec3919aadcd8
Maximillian Rubensson <maximillian.rubensson@iths.se>
2025-04-11 09:43:36 +0200

    refactor how prompting is done

--- src/main/java/org/storkforge/barkr/ai/controller/MistralAiController.java
+++ src/main/java/org/storkforge/barkr/ai/controller/MistralAiController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ai.mistralai.MistralAiChatModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.storkforge.barkr.exceptions.InvalidApiKey;
@@ -14,7 +15,10 @@ import java.util.Map;
 @RestController
 public class MistralAiController {
     private static final Logger logger = LoggerFactory.getLogger(MistralAiController.class);
-    String gen = "generation";
+    String genKey = "generation";
+
+    @Value("${ai.mistral.joke-prompt}")
+    private String jokePrompt;
 
     private final MistralAiChatModel chatModel;
 
@@ -26,17 +30,17 @@ public class MistralAiController {
     @GetMapping("/ai/generate")
     public Map<String, String> generate() {
         try {
-            String response = this.chatModel.call("tell me a dog joke like you are a dog and dont write anything else and dont explain the joke");
-            return Map.of(gen, response);
+            String response = this.chatModel.call(jokePrompt);
+            return Map.of(genKey, response);
         } catch (InvalidApiKey e) {
             logger.error("Invalid API Key: {}", e.getMessage(), e);
-            return Map.of(gen, "Invalid API key. Please check your configuration.");
+            return Map.of(genKey, "Invalid API key. Please check your configuration.");
         } catch (ServerUnavailable e) {
             logger.error("AI server is down: {}", e.getMessage(), e);
-            return Map.of(gen, "The AI server is currently unavailable. Please try again later.");
+            return Map.of(genKey, "The AI server is currently unavailable. Please try again later.");
         }catch (Exception e) {
             logger.error("Unexpected error generating AI response: {}", e.getMessage(), e);
-            return Map.of(gen, "Sorry, I couldn't generate a response. Please try again later.");
+            return Map.of(genKey, "Sorry, I couldn't generate a response. Please try again later.");
         }
     }
 
