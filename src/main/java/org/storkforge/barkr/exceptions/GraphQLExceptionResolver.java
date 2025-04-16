286e726c628966eb1d30132985abb334f2982d9f
voffie <viktorsoderborg@gmail.com>
2025-04-16 13:17:10 +0200

    fix: update graphql error handling

--- src/main/java/org/storkforge/barkr/exceptions/GraphQLExceptionResolver.java
+++ src/main/java/org/storkforge/barkr/exceptions/GraphQLExceptionResolver.java
@@ -3,6 +3,7 @@ package org.storkforge.barkr.exceptions;
 import graphql.GraphQLError;
 import graphql.GraphqlErrorBuilder;
 import graphql.schema.DataFetchingEnvironment;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter;
 import org.springframework.graphql.execution.ErrorType;
 import org.springframework.stereotype.Component;
@@ -18,8 +19,20 @@ public class GraphQLExceptionResolver extends DataFetcherExceptionResolverAdapte
              .path(env.getExecutionStepInfo().getPath())
              .location(env.getField().getSourceLocation())
              .build();
+    } else if (ex instanceof ConstraintViolationException) {
+      return GraphqlErrorBuilder.newError()
+              .errorType(ErrorType.BAD_REQUEST)
+              .message(ex.getMessage())
+              .path(env.getExecutionStepInfo().getPath())
+              .location(env.getField().getSourceLocation())
+              .build();
     } else {
-      return null;
+      return GraphqlErrorBuilder.newError()
+              .errorType(ErrorType.INTERNAL_ERROR)
+              .message("An unexpected error occurred.")
+              .path(env.getExecutionStepInfo().getPath())
+              .location(env.getField().getSourceLocation())
+              .build();
     }
   }
 }
